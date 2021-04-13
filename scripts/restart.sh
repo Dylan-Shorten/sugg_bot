367bfb57d76b7fcd435349de926b5988b4a2f31f
FinnPerry <finnperry3@gmail.com>
2021-04-13 23:40:19 +0000

    Add discord connection
    new: bot now connects to discord
    new: add restart script

---
+++ scripts/restart.sh
@@ -0,0 +1,2 @@
+./kill.sh
+./start.sh
--- sugg_bot/sb_main.py
+++ sugg_bot/sb_main.py
@@ -1,6 +1,23 @@
+import pathlib
+import shlex
+import discord
+
+def load_token():
+    token = pathlib.Path('../data/token.txt').read_text()
+    return token.replace('\n', '')
+
 def main():
-    while True:
-        continue
+    client = discord.Client()
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        words = shlex.split(message.content)
+        if words[0] == 'sb':
+            if words[1] == 'ping':
+                await message.channel.send('pong')
+    token = load_token()
+    client.run(token)
 
 if __name__ == '__main__':
     main()
