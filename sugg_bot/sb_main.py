3e8a9743e22f3174ef8025b610a4a44aed87d778
FinnPerry <finnperry3@gmail.com>
2021-04-14 00:27:28 +0000

    pylint
    fmt: Conform to pylint

--- sugg_bot/sb_main.py
+++ sugg_bot/sb_main.py
@@ -1,14 +1,19 @@
+'''sugg discord bot'''
+
 import pathlib
 import shlex
 import discord
 
 def load_token():
+    '''load discord token'''
     token = pathlib.Path('../data/token.txt').read_text()
     return token.replace('\n', '')
 
 def main():
+    '''main'''
     client = discord.Client()
     @client.event
+    # pylint: disable=unused-variable
     async def on_message(message):
         if message.author == client.user:
             return
