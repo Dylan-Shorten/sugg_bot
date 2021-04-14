605f6615cb1fac7907a2a305ec7aa0f82e47207e
FinnPerry <finnperry3@gmail.com>
2021-04-14 06:20:43 +0000

    Add echo command

--- sugg_bot/sb_commands.py
+++ sugg_bot/sb_commands.py
@@ -12,6 +12,23 @@ async def command_ping(opts, args, channel):
         return
     await channel.send('pong')
 
+async def command_echo(opts, args, channel):
+    '''command echo'''
+    if len(args) < 1:
+        await channel.send('echo takes 1 or more args')
+        return
+    string = ''
+    for arg in args:
+        string += arg + ' '
+    string = string[:-1]
+    for (opt, arg) in opts:
+        if opt == '-l':
+            string = string.lower()
+        elif opt == '-u':
+            string = string.upper()
+    await channel.send(string)
+
 COMMANDS = {
-        'ping': Command('', [], command_ping)
+        'ping': Command('', [], command_ping),
+        'echo': Command('lu', [], command_echo)
         }
